f9e423d3c6d9e443d4b5fb9590a693e250d9a5ac
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-01-10 21:25:31 +0100

    historycontroller

---
+++ Controllers/historyController/histroy.controller.js
@@ -0,0 +1,11 @@
+const gameHandler = require('../../handlerGame/response.controller');
+
+const getHistory = require('./getHistory');
+
+
+
+module.exports = {
+    getHistory: (req,res) => {
+        getHistory.getHistory(res,req.body.hashId);
+    },
+}

ca2a903583a45b1db4a7151d5247a2e0b77915c6
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-01-10 21:18:30 +0100

    click cel

--- Controllers/gameController/click.cel.js
+++ Controllers/gameController/click.cel.js
@@ -1,32 +1,53 @@
 const gameHandler = require('../../handlerGame/response.controller');
 
+const gameModel = require('../../Models/game');
 
-module.exports.clickCel = async (res,game,rowIndex,colIndex) => {
-        let cel = game.matrix[rowIndex][colIndex];
-        if (!cel.clicked && game.playing && !game.completed ) {
-            game.userClick += 1;
-            if (cel.color == 'green'){
-                cel.clicked = true;
-                    res.json({msg : 'YOU HAVE CLICK RIGHT CELL',success: true,status : 200,response: {userClick: game.userClick, color: 'green'}});
+const hash = require('../../Models/hash');
 
+module.exports.clickCel = async (res,gameHash,gameId,rowIndex,colIndex) => {
+    gameModel.findOne({hash:gameHash,_id: gameId})
+        .then(async (game) => {
+            if (game){
+                let cel = game.matrix[rowIndex][colIndex];
+                if (!cel.clicked && game.playing && !game.completed ) {
+                    game.userClick += 1;
+                    if (cel.color == 'green'){
+                        cel.clicked = true;
+                            res.json({msg : 'YOU HAVE CLICK RIGHT CELL',success: true,status : 200,response: {userClick: game.userClick, color: 'green'}});
+        
+                    }
+                    else {
+                        cel.clicked = true;
+                        const indexMines = await loseGame(game);
+                        res.json({msg : 'YOU HAVE CLICK MINE CELL',success: true,status : 200,response: {userClick: game.userClick,indexMines: indexMines,color: 'red'}});
+                    }
+                    game.matrix[rowIndex][colIndex] = cel;
+                    gameModel.updateOne({_id:gameId},game)
+                        .then((updated => {
+                        }))
+                }
             }
+
             else {
-                cel.clicked = true;
-                const indexMines = await loseGame();
-                    res.json({msg : 'YOU HAVE CLICK MINE CELL',success: true,status : 200,response: {userClick: game.userClick,indexMines: indexMines,color: 'red'}});
+                res.json({msg : 'YOU HAVE NO GAME',success: true,status : 404,response: null});
             }
-            game.matrix[rowIndex][colIndex] = cel;
-        }
+            
+        })
+        .catch(err => {
+            console.log(err);
+            res.json({msg : 'SOMETHING WENT WRONG',success: true,status : 500,response: null});
+        })
+        
     
         
 }
 
 
-async function loseGame() {
+async function loseGame(game) {
     let indexMines = [];
-    this.game.completed = true;
-    this.game.playing = false;
-    await this.game.matrix.map((row,indexRow) => {
+    game.completed = true;
+    game.playing = false;
+    await game.matrix.map((row,indexRow) => {
         row.map((col,indexCol) => {
             if (col.color == 'red'){
                 indexMines.push({indexRow : indexRow, indexCol : indexCol});
--- Controllers/gameController/game.controller.js
+++ Controllers/gameController/game.controller.js
@@ -5,7 +5,7 @@ const clickCel = require('./click.cel');
 const createGame = require('./game.create');
 
 const checkGame = require('./checkGame');
-let games = {}
+
 module.exports = {
     createGame: (req,res) => {
         createGame.createGame(res,req.body.gameHash,req.body.game);
@@ -14,7 +14,7 @@ module.exports = {
     clickCel: (req,res) => {
         const gameId = req.body.gameId;
         const gameHash = req.body.gameHash;
-        clickCel.clickCel(res,games[gameHash][gameId],req.body.rowIndex,req.body.colIndex);
+        clickCel.clickCel(res,gameHash,gameId,req.body.rowIndex,req.body.colIndex);
     },
 
     checkGame : (req,res) => {

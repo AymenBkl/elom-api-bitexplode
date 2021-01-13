8a598204ed53205605d80e25f5fdd5648990bc3d
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-01-13 23:08:30 +0100

    working ssl mongodb

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports.config = {
     webURL: 'https://localhost:8100',  
     mongoDB: {
             url : 'mongodb://192.168.1.104:27017/bitexplode?&ssl=true',
-            user: 'bitExplodePlayer',
+            user: 'bitExplodePlayer', 
             pwd: '$Kd%qP-aG8PLW*GYrvz5La&cWDf_Us$QHf5maBye*^dC78M#wX2cvxp=&pYtBm=BV!6JjjvJY^zV6kK&=F-^?j9YpagwphzQmDGZfCr?Sw2Z3%+AWCyam4QJ5j^7rxygEFyAz9JdPem#qB-JzaH!?S-DF_AU_HYPDJPTCE__LQAWMVaS4aJGus-dkmwTj+rCH%@g^^M-tG58&?Gu=PF_8LuXjagJK2%%$%%3c6a#ZzUt-2d22D+snE75Yc_wP#gR'
     }
 } 

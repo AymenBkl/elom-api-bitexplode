c393e3d5af0913c6db260e69e95fc4155d00d560
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-01-13 02:38:43 +0100

    ssl working

--- Middlewares/mongoose.js
+++ Middlewares/mongoose.js
@@ -1,8 +1,27 @@
 const mongoose = require("mongoose");
+const fs = require('fs');
+
+
 
 const config = require('../config');
+
+var key = fs.readFileSync(process.mainModule.path + '\\mongoSSL\\mongodb.pem');
+
+var ca = fs.readFileSync(process.mainModule.path + '\\mongoSSL\\rootCA.pem');
+
+
+var options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+    sslValidate:false,
+    sslCA: ca,
+    sslCert:key,
+    sslKey:key,
+};
+
 module.exports = mongoose
-  .connect(config.config.mongoURL, { useNewUrlParser: true,useFindAndModify : false })
+  .connect(config.config.mongoURL,options)
   .then((db) => {
     console.log("connected to db");
   })
--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports.config = {
     url : 'http://192.168.1.104:3000',
     https: 'https://192.168.1.104:3000',
     webURL: 'https://localhost:8100',
-    mongoURL : 'mongodb://localhost:27017/bitexplode',
+    mongoURL : 'mongodb://192.168.1.104:27017/bitexplode?&ssl=true',
 }

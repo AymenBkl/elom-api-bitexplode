f6941cabc32f39fddfb3ca28e7ef65bc36e5f0fe
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-03-12 13:16:21 +0100

    click cell

--- Controllers/gameController/game.create.js
+++ Controllers/gameController/game.create.js
@@ -12,31 +12,41 @@ const deposit = require('../../Models/deposit');
 
 module.exports.createGame = async (res, hashId, game, addressId) => {
   checkStake(addressId, game.stake)
-  /**let gameToCreate = {
-    hash: hashId,
-    stake: game.stake,
-    numberMines: game.numberMines,
-    userClick: 0,
-    playing: true,
-    completed: false,
-  }
-  let matrix = await createMatrix(game.numberMines)
-  gameToCreate.matrix = matrix.game;
-  gameToCreate.data = matrix.data;
-  gameModel.create(gameToCreate)
-    .then((gameCreated) => {
-      if (gameCreated) {
-        console.log(gameCreated);
-        insertGameToHash(res, hashId, gameCreated);
+    .then(async (result) => {
+      if (result && result.state != false) {
+        let gameToCreate = {
+          hash: hashId,
+          stake: game.stake,
+          numberMines: game.numberMines,
+          userClick: 0,
+          playing: true,
+          completed: false,
+        }
+        let matrix = await createMatrix(game.numberMines)
+        gameToCreate.matrix = matrix.game;
+        gameToCreate.data = matrix.data;
+        gameModel.create(gameToCreate)
+          .then((gameCreated) => {
+            if (gameCreated) {
+              console.log(gameCreated);
+              insertGameToHash(res, hashId, gameCreated);
+            }
+            else {
+              gameHandler.response("error", res, "YOUR GAME COULDNLT CREATE", 404);
+            }
+          })
+          .catch(err => {
+            console.log(err);
+            gameHandler.response("error", res, "SOMETHING WENT WRONG", 500);
+          })
       }
       else {
-        gameHandler.response("error", res, "YOUR GAME COULDNLT CREATE", 404);
+        gameHandler.response("error", res, result.msg, 404);
       }
     })
     .catch(err => {
-      console.log(err);
-      gameHandler.response("error", res, "SOMETHING WENT WRONG", 500);
-    })**/
+      gameHandler.response("error", res, "Something Went Wrong !",500);
+    })
 }
 
 async function createMatrix(numberMines) {
@@ -60,62 +70,76 @@ async function createMine(game, numberMines) {
 };
 
 function checkStake(addressId, stake) {
-  deposit.find(
-    {
-      addressId: addressId
-    }
-  )
-  .sort('-createdAt')
-    .then((deposits) => {
-      console.log(deposits)
-      /**if (deposits && deposits.length > 0) {
-        let depositIds = [];
-        let currentStake = stake;
-        let index = 0;
-        while (index < deposits.length) {
-          if (deposits[index].currentBalance >= currentStake){
-            deposits[index].currentBalance -= currentStake;
-            currentStake = 0;
-            depositIds.push(deposits[index]);
-            index = deposits.length + 1;
-          } 
+  return new Promise((resolve,reject) => {
+    deposit.find(
+      {
+        addressId: addressId,
+        currentBalance: {
+          $gt:0
+        }
+      }
+    )
+    .sort('-createdAt')
+      .then(async (deposits) => {
+        if (deposits && deposits.length > 0) {
+          let depositIds = [];
+          let currentStake = stake;
+          let index = 0;
+          while (index < deposits.length) {
+            if (deposits[index].currentBalance >= currentStake){
+              deposits[index].currentBalance -= currentStake;
+              currentStake = 0;
+              depositIds.push(deposits[index]);
+              index = deposits.length + 1;
+            } 
+            else {
+              currentStake -= deposits[index].currentBalance;
+              deposits[index].currentBalance = 0;
+              depositIds.push(deposits[index]);
+              index += 1;
+            }
+          }
+          if (currentStake == 0){
+            resolve(updateDeposits(depositIds));
+          }
           else {
-            currentStake -= deposits[index].currentBalance;
-            deposits[index].currentBalance = 0;
-            depositIds.push(deposits[index]);
-            index += 1;
+            resolve({statue:false,msg: 'You don"t have enough balance'});
           }
         }
-        if (currentStake == 0){
-          updateDeposits(depositIds);
-        }
-      }**/
-    })
+      })
+      .catch(err => {
+        reject(err);
+      })
+  })
+  
 }
 
 function updateDeposits(depositIds){
-  console.log(depositIds)
-  deposit.bulkWrite(
-    depositIds.map((deposit) => 
-    ({
-        updateOne: {
-            filter: { _id: deposit._id },
-            update: {
-                $set: {
-                    currentBalance: deposit.currentBalance
-                },
-            },
-            new: true, setDefaultsOnInsert: true 
-        }
-    })
-    )
-)
-    .then(deposit => { 
-        console.log(deposit);
-    })
-    .catch(err => {
-        console.log(err);
-    })
+  return new Promise((resolve,reject) => {
+    deposit.bulkWrite(
+      depositIds.map((deposit) => 
+      ({
+          updateOne: {
+              filter: { _id: deposit._id },
+              update: {
+                  $set: {
+                      currentBalance: deposit.currentBalance
+                  },
+              },
+              new: true, setDefaultsOnInsert: true 
+          }
+      })
+      )
+  )
+      .then(deposit => { 
+          console.log(deposit);
+          resolve({status:true});
+      })
+      .catch(err => {
+          reject(err);
+      })
+  })
+  
 }
 function insertGameToHash(res, hashId, currentGame) {
   hash.findByIdAndUpdate(hashId, {

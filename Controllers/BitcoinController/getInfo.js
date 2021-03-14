fddf7ab770336f2ed9f22bf49ad7b0fba2d64143
Aymen Abdelmoumen Boukhelkhal <a.boukhalkhel@esi-sba.dz>
2021-03-14 15:09:16 +0100

    fixed real time balance

--- Controllers/BitcoinController/getInfo.js
+++ Controllers/BitcoinController/getInfo.js
@@ -5,8 +5,12 @@ module.exports.getInfo = (url,dataString) => {
     return new Promise((resolve,reject) => {
         callback = (error, response, body) => {
           if (error || response.statusCode != 200){
-            console.log(response);
-            resolve({err:"Something Went Wrong",method:url,status:response.statusCode,message:JSON.parse(body)});
+            if (response) {
+              resolve({err:"Something Went Wrong",method:url,status:response.statusCode,message:JSON.parse(body)});
+            }
+            else {
+              resolve({err:"Something Went Wrong",method:url,status:500,message:"Something Went Wrong"});
+            }
           }
           if (!error && response.statusCode == 200) {
             const data = JSON.parse(body);
